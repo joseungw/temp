f5d086480aa723b2bc30af04a3f0d67e8dbe3ff9
joseungw <101791486+joseungw@users.noreply.github.com>
2022-07-25 17:04:42 +0900

    Add files via upload

---
+++ 포트폴리오/java/beans/User.java
@@ -0,0 +1,56 @@
+package beans;
+
+public class User {
+	private String name;
+	private String phone;
+	private String id;
+	private String password;
+	private String nick;
+	private String city;
+	private String sports;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getNick() {
+		return nick;
+	}
+	public void setNick(String nick) {
+		this.nick = nick;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getSports() {
+		return sports;
+	}
+	public void setSports(String sports) {
+		this.sports = sports;
+	}
+	
+	
+}
