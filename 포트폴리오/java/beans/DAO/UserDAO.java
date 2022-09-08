62277c7f8854ad437f9a41996ea60e98eee9a8b0
joseungw <101791486+joseungw@users.noreply.github.com>
2022-09-08 16:23:34 +0900

    Add files via upload

--- 포트폴리오/java/beans/DAO/UserDAO.java
+++ 포트폴리오/java/beans/DAO/UserDAO.java
@@ -1,7 +1,10 @@
 package DAO;
 
 import java.sql.*;
+import java.util.ArrayList;
+
 import beans.User;
+import beans.Notice;
 
 public class UserDAO {
 	private UserDAO() {
@@ -10,9 +13,9 @@ public class UserDAO {
 	public static UserDAO instance=new UserDAO();
 	
 	public static UserDAO getInstance() {
-		return instance;			//ì‹±ê¸€í†¤
+		return instance;			//½Ì±ÛÅæ
 	}
-	//ì»¤ë„¥ì…˜ ì—°ê²° ì²˜ë¦¬ë¬¸
+	//Ä¿³Ø¼Ç ¿¬°á Ã³¸®¹®
 	public Connection getConnection() throws Exception {
 		Connection conn=null;
 		String url="jdbc:mysql://127.0.0.1:3306/project";
@@ -22,7 +25,7 @@ public class UserDAO {
 		conn=DriverManager.getConnection(url,dbid,dbpw);
 		return conn;
 	}
-	//ë¡œê·¸ì¸ ì¸ì¦ ì²˜ë¦¬
+	//·Î±×ÀÎ ÀÎÁõ Ã³¸®
 	public int userCheak(String id,String password) {
 		int result=-1;
 		String sql="select password from user where id=?;";
@@ -34,14 +37,14 @@ public class UserDAO {
 			pstmt=conn.prepareStatement(sql);
 			pstmt.setString(1,id);
 			rs=pstmt.executeQuery();
-			if(rs.next()) {	//í•´ë‹¹ ì•„ì´ë””ê°€ ì¡´ì¬ í•œë‹¤ë©´
-				if(rs.getString("password").equals(password) && rs.getString("password")!=null) {	//ë¹„ë°€ë²ˆí˜¸ê°€ ì¼ì¹˜ í•œë‹¤ë©´
-					result=1;	//ì•„ì´ë”” ë¹„ë°€ë²ˆí˜¸ ì¼ì¹˜í•˜ëŠ” ê²½ìš°
+			if(rs.next()) {	//ÇØ´ç ¾ÆÀÌµğ°¡ Á¸Àç ÇÑ´Ù¸é
+				if(rs.getString("password").equals(password) && rs.getString("password")!=null) {	//ºñ¹Ğ¹øÈ£°¡ ÀÏÄ¡ ÇÑ´Ù¸é
+					result=1;	//¾ÆÀÌµğ ºñ¹Ğ¹øÈ£ ÀÏÄ¡ÇÏ´Â °æ¿ì
 				}else {
-					result=0;	//ë¹„ë°€ë²ˆí˜¸ê°€ ì¼ì¹˜í•˜ì§€ ì•Šì€ ê²½ìš°
+					result=0;	//ºñ¹Ğ¹øÈ£°¡ ÀÏÄ¡ÇÏÁö ¾ÊÀº °æ¿ì
 				}
 			}else {
-				result=-1;		//ì•„ì´ë””ê°€ ì¼ì¹˜í•˜ì§€ ì•Šì€ ê²½ìš°
+				result=-1;		//¾ÆÀÌµğ°¡ ÀÏÄ¡ÇÏÁö ¾ÊÀº °æ¿ì
 			}
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -57,7 +60,7 @@ public class UserDAO {
 		}
 		return result;
 	}
-	//ì‚¬ìš©ì ì •ë³´ ì¡°íšŒ
+	//»ç¿ëÀÚ Á¤º¸ Á¶È¸
 	public User getUser(String id) {
 		User u=new User();
 		String sql="select * from user where id=?";
@@ -71,6 +74,7 @@ public class UserDAO {
 			rs=pstmt.executeQuery();
 			rs.next();
 			u.setId(rs.getString("id"));
+			u.setNick(rs.getString("nick"));
 		}catch(Exception e) {
 			e.printStackTrace();
 		}finally {
@@ -83,5 +87,45 @@ public class UserDAO {
 			}
 		}
 		return u;
+	}
+	//°Ô½ÃÆÇ Ä«Å×°í¸®·Î ºĞ·ù ÇØ¼­ º¸¿©ÁÖ±â ºÎºĞ
+	public ArrayList<Notice> getNotiCate(String category) {
+		ArrayList<Notice> list=new ArrayList<Notice>();
+		Notice nt=null;
+		String sql="select * from Notice where category=?";
+		
+		Connection conn=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
+		try {
+			conn=getConnection();
+			pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, category);
+			rs=pstmt.executeQuery();
+			while(rs.next()) {
+				nt=new Notice();
+				nt.setNotenum(rs.getInt("notenum"));
+				nt.setUserid(rs.getString("userid"));
+				nt.setUsernick(rs.getString("usernick"));
+				nt.setCategory(rs.getString("category"));
+				nt.setNotedate(rs.getString("notedate"));
+				nt.setTitle(rs.getString("title"));
+				nt.setContents(rs.getString("contents"));
+				nt.setRegion(rs.getString("region"));
+				list.add(nt);
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null)conn.close();
+			}catch(Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		return list;
 	}
 }
